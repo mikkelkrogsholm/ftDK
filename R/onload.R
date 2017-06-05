e220863e880f0c8c1b85cc3fc27cdcc5aa2a607e
Mikkel Freltoft Krogsholm <mikkel@56n.dk>
2017-06-05 19:08:58 +0000

    Added descriptions to the endpoints data frame Added a startup message with the Parliaments conditions for use of the data.

---
+++ R/onload.R
@@ -0,0 +1,3 @@
+.onLoad <- function(libname, pkgname){
+  packageStartupMessage(startup_message)
+}
--- dev/endpoints.R
+++ dev/endpoints.R
@@ -1,3 +1,4 @@
+library(rvest)
 
 url <- "http://oda.ft.dk/api/"
 
@@ -15,10 +16,35 @@ api_endpoints <- purrr::map_df(api_list, function(x){
   )
 })
 
+api_endpoints$original <- api_endpoints$endpoint
 
 api_endpoints$endpoint <- tolower(api_endpoints$endpoint)
 api_endpoints$endpoint <- stringr::str_replace_all(api_endpoints$endpoint, "æ", "ae")
 api_endpoints$endpoint <- stringr::str_replace_all(api_endpoints$endpoint, "ø", "oe")
 api_endpoints$endpoint <- stringr::str_replace_all(api_endpoints$endpoint, "å", "aa")
+
+
+url <- "dev/html/Oda.htm"
+
+html_data  <- read_html(url)
+
+endpoint <- html_data %>%
+  html_nodes(".withEntityDescription") %>%
+  html_text(trim = T)
+
+description <- html_data %>%
+  html_nodes(".withEntityDescription") %>%
+  html_attr("data-description")
+
+desc <- tibble::tibble(
+  original = endpoint,
+  description = description
+)
+
+###
+
+df <-  dplyr::left_join(api_endpoints, desc)
+
+api_endpoints <-  df
 
 devtools::use_data(api_endpoints, overwrite = T)
