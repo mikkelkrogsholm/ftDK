e9e2901cb554fefc3c7960f974ab92035baf095a
emillykkejensen <emil@emillykkejensen.dk>
2019-09-22 07:37:04 +0000

    Added verbose, so you can trun of messages during function call

--- R/functions.R
+++ R/functions.R
@@ -41,20 +41,28 @@ see_endpoints <- function(){
 #'
 #' @param endpoint is the endpoint in the API that you want data from.
 #' @param extraargs is aditional arguments to be used when calling the API - eg. filter
+#' @param verbose if TRUE (default) tells you what is going on
 #'
 #' @return a data frame with the data requested
 #' @export
+#'
+#' @details
+#'
+#' See posible aditional arguments at \url{https://oda.ft.dk/Home/OdataQuery}
+#'
+#' To get a list of posible operators to use with the aditional arguments see
+#' \url{https://www.odata.org/documentation/odata-version-2-0/uri-conventions/}
 #'
 #' @examples
 #'
 #'\dontrun{
 #' library(ftDK)
 #' see_endpoints()
-#' ft_data  <- get_ft("afstemning")
+#' ft_data  <- get_ft("afstemning", "&$filter=vedtaget%20eq%20true")
 #' ft_data
 #' }
 
-get_ft <- function(endpoint, extraargs = NULL){
+get_ft <- function(endpoint, extraargs = NULL, verbose = TRUE){
 
   # Make local copy of the endpoints data frame
   endpoint_df <- ftDK::api_endpoints
@@ -78,10 +86,9 @@ get_ft <- function(endpoint, extraargs = NULL){
   seqnr <- seq(0, ceiling(odata.count/20)*20, 20)
 
   # User feedback
-  message(paste("Fetching", odata.count, "rows of data"))
+  if(verbose) message(paste("Fetching", odata.count, "rows of data"))
 
-  # Use an lapply loop to fetch data
-  ft_data_list <- pbapply::pblapply(seqnr, function(i){
+  lapply_loop_func <- function(i){
 
     url <- paste0(base_url, "&$skip=", as.integer(i))
 
@@ -91,10 +98,14 @@ get_ft <- function(endpoint, extraargs = NULL){
     value_df <- purrr::map_df(value, function(x) tibble::as_tibble(x))
 
     return(value_df)
-  })
+
+  }
+
+  # Use an lapply loop to fetch data
+  ft_data_list <- if(verbose) pbapply::pblapply(seqnr, lapply_loop_func) else lapply(seqnr, lapply_loop_func)
 
   # Put all data into one data frame
-  message("Putting it all in to one data frame")
+  if(verbose) message("Putting it all in to one data frame")
   ft_data_df <- dplyr::bind_rows(ft_data_list)
 
   # Return the data
